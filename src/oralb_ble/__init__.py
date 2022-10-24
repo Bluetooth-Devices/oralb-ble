ff388fc4a6fa2748247d8c399bd8f38c2682ace4
J. Nick Koston <nick@koston.org>
2022-10-24 18:05:50 -0500

    feat: init parser

--- src/oralb_ble/__init__.py
+++ src/oralb_ble/__init__.py
@@ -1,7 +1,7 @@
 """Parser for OralB BLE advertisements.
 
 This file is shamelessly copied from the following repository:
-https://github.com/Ernst79/bleparser/blob/c42ae922e1abed2720c7fac993777e1bd59c0c93/package/bleparser/oralb.py
+https://github.com/Ernst79/bleparser/blob/c42ae922e1abed2720c7fac993777e1bd59c0c93/package/bleparser/oral_b.py
 
 MIT License applies.
 """
@@ -19,7 +19,7 @@ from sensor_state_data import (
 
 from .parser import OralBBluetoothDeviceData
 
-__version__ = "0.19.0"
+__version__ = "0.0.1"
 
 __all__ = [
     "OralBBluetoothDeviceData",
