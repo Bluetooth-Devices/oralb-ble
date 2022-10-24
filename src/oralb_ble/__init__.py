babff0c9cd6106f69eb79e7cd6003456476d5888
J. Nick Koston <nick@koston.org>
2022-10-24 17:53:40 -0500

    feat: init repo

--- src/oralb_ble/__init__.py
+++ src/oralb_ble/__init__.py
@@ -1,1 +1,34 @@
-__version__ = "0.0.1"
+"""Parser for Govee BLE advertisements.
+
+This file is shamelessly copied from the following repository:
+https://github.com/Ernst79/bleparser/blob/c42ae922e1abed2720c7fac993777e1bd59c0c93/package/bleparser/govee.py
+
+MIT License applies.
+"""
+from __future__ import annotations
+
+from sensor_state_data import (
+    DeviceClass,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+
+from .parser import GoveeBluetoothDeviceData
+
+__version__ = "0.19.0"
+
+__all__ = [
+    "GoveeBluetoothDeviceData",
+    "SensorDescription",
+    "SensorDeviceInfo",
+    "DeviceClass",
+    "DeviceKey",
+    "SensorUpdate",
+    "SensorDeviceInfo",
+    "SensorValue",
+    "Units",
+]
--- tests/test_main.py
+++
@@ -1,5 +0,0 @@
-from oralb_ble.main import add
-
-
-def test_add():
-    assert add(1, 1) == 2
